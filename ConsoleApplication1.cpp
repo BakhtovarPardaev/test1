fd084f386f5d6571c5862684099a3f965baadaa0
BakhtovarPardaev <bahp2003@gmail.com>
2023-02-15 00:54:22 +0300

    first

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,55 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+class  human
+{public:
+    static int kol;
+
+    human()
+    {     
+       
+           
+           if (kol > 4)
+           {
+            cout<<"oshibka"<<endl;
+            return;
+           }
+           kol++;
+           Id = kol;
+          
+    }
+
+    ~human()
+    {
+
+    }
+
+   int Id;
+};
+
+int human::kol = 0;
+
+int main()
+{
+    human a;
+    human b;
+    human c;
+    human v;
+    human d;
+    human q;
+
+    
+    cout << a.Id << endl;
+    cout << b.Id << endl;
+    cout << c.Id << endl;
+    cout << v.Id << endl;
+    cout << d.Id << endl;
+    cout << q.Id << endl;
+
+    cout <<endl<< human::kol << endl;
+
+}
+
+
